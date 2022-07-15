f5816232dc1583d6c04459d1bdf2499deea73281
ImssyTnT <562285429@qq.com>
2022-07-15 17:53:25 +0800

    完成了布局页面路由功能

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,20 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [{ path: '/login', component: () => import('@/views/Login') }]
+const routes = [
+  { path: '/login', component: () => import('@/views/Login') },
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      { path: ' ', component: () => import('@/views/Home') },
+      { path: 'video', component: () => import('@/views/Video') },
+      { path: 'qa', component: () => import('@/views/Qa') },
+      { path: 'profile', component: () => import('@/views/Profile') }
+    ]
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!-- 二级路由 -->
+    <router-view></router-view>
+
+    <!-- 底部导航 -->
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <span class="toutiao toutiao-shouye"></span>
+        </template>
+        首页
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <span class="toutiao toutiao-shipin"></span>
+        </template>
+        视频
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <span class="toutiao toutiao-wenda"></span>
+        </template>
+        问答
+      </van-tabbar-item>
+      <van-tabbar-item to="/profile">
+        <template #icon>
+          <span class="toutiao toutiao-wode"></span>
+        </template>
+        我的
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.van-tabbar-item {
+  justify-content: space-evenly;
+  :deep(.van-tabbar-item__icon) {
+    font-size: 0.32rem;
+    margin: 0;
+    .toutiao {
+      font-size: 0.53333rem;
+    }
+  }
+}
+</style>
